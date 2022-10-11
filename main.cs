3482aed4c00728fa7df1ba701c003d8a0d547105
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2022-10-11 18:10:09 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,19 @@
+// Created on: Oct 2022
+// Created by: liya getachew
+//
+// This program does basic math
+
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("The dimensions of a rectangle are: 6 cm and 6 cm.");
+        Console.WriteLine("The area is: " + (6 * 6) + "cm²");
+        Console.WriteLine("The perimeter is: " + (2 * (6 + 6)) + "cm");
+
+        Console.WriteLine("\nDone");
+    }
+}
